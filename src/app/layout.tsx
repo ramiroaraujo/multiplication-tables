b37c2cbe7a26b61a97504d340814780c9ff0298a
Ramiro Araujo <rama.araujo@gmail.com>
2024-10-07 09:22:59 -0300

    update title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
         />
+        <title>Tablas de Multiplicar</title>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
